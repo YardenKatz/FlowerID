c3c5c8d691e013d31487f7e141430d86f8758610
Yarden Katz <yardenk@balink.net>
2021-07-12 09:25:20 +0300

    using data model to identify images

--- FlowerID/FlowerID/ViewController.swift
+++ FlowerID/FlowerID/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var bgImageView: UIImageView!
     let picker = UIImagePickerController()
+    var pickedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +26,40 @@ class ViewController: UIViewController {
     @IBAction func didPressCamera(_ sender: Any) {
         present(picker, animated: true, completion: nil)
     }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier(configuration: MLModelConfiguration()).model) else {
+            fatalError("Cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            let classification = request.results?.first as? VNClassificationObservation
+            self.navigationItem.title = classification?.identifier
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 }
 
 // MARK:- UIImagePickerControllerDelegate
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true, completion: nil)
-        guard let image = info[.originalImage] as? UIImage else { return }
         
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[.originalImage] as? UIImage else { return }
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Could not convert image to CIIImage.")
+        }
+        
+        pickedImage = image
+        detect(image: ciImage)
         bgImageView.image = image
     }
 }
